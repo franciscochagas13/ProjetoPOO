41c558ef5fd0a0807a62767cc78d1b6fba85733f
leonardopatriciocg <71447815+leonardopatriciocg@users.noreply.github.com>
2020-11-24 18:47:32 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -2,4 +2,26 @@ package gerenciadorVacinas;
 
 public class Usuario {
 
+		
+		private int rg;
+		private String nome;	
+		
+		public int getRG() {
+			return rg;
+		}
+		public void setRG(int Rg) {
+			rg = Rg;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+		
+		
+		
+
+	
 }
