3f4a4a7253e11b07cf0e691539770d48ca9087e9
leonardopatriciocg <71447815+leonardopatriciocg@users.noreply.github.com>
2020-11-24 18:46:24 -0300

    Update Endereco.java

--- Endereco.java
+++ Endereco.java
@@ -1,5 +1,37 @@
 package gerenciadorVacinas;
 
 public class Endereco {
+	
+	
+	private String rua;
+	private String cidade;
+	private String numeroCasa;
+	private int cep;
+	
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getNumeroCasa() {
+		return numeroCasa;
+	}
+	public void setNumeroCasa(String numeroCasa) {
+		this.numeroCasa = numeroCasa;
+	}
+	public int getCep() {
+		return cep;
+	}
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+	
 
 }
